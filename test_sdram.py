02026939e5748bcedf7dada9f695f8a3c76800cb
Jędrzej Boczar <jboczar@antmicro.com>
2020-07-07 12:23:05 +0200

    extend test_sdram.py script for more interactive use

--- test_sdram.py
+++ test_sdram.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 
 from litex import RemoteClient
 
 from sdram_init import *
 
-wb = RemoteClient(debug=False)
-wb.open()
 
-# # #
+def parse_args():
+    parser = argparse.ArgumentParser(description="Test SDRAM")
+    parser.add_argument("-i", "--interleaved", action="store_true",
+                        help="Interleave writes and reads")
+    parser.add_argument("-l", "--length", default="64", help="Length of test")
+    parser.add_argument("-s", "--start",  default="0",  help="Number of start word")
+    parser.add_argument("-r", "--read-only", action="store_true", help="Number of start word")
+    parser.add_argument("--random", action="store_true", help="Random data")
+    parser.add_argument("--no-init", action="store_true", help="No SDRAM initialization")
+    parser.add_argument("--no-main", action="store_true", help="Don't run main")
+    return parser.parse_args()
 
-# Tests---------------------------------------------------------------------------------------------
-sdram_initialization  = True
-sdram_test            = True
-
-# Parameters----------------------------------------------------------------------------------------
-
-N_BYTE_GROUPS = 2
-NDELAYS       = 8
-
-# MPR
-MPR_PATTERN = 0b01010101
-
-# MR3
-MPR_SEL   = (0b00<<0)
-MPR_ENABLE = (1<<2)
-
-# Software helpers/models---------------------------------------------------------------------------
+# Initialization -----------------------------------------------------------------------------------
 
 def sdram_software_control():
     wb.regs.sdram_dfii_control.write(0)
@@ -40,12 +33,9 @@ def sdram_mr_write(reg, value):
     wb.regs.sdram_dfii_pi0_command.write(dfii_command_ras | dfii_command_cas | dfii_command_we | dfii_command_cs)
     wb.regs.sdram_dfii_pi0_command_issue.write(1)
 
-# software control
-sdram_software_control()
+def sdram_init(wb):
+    sdram_software_control()
 
-# sdram Initialization------------------------------------------------------------------------------
-
-if sdram_initialization:
     for i, (comment, a, ba, cmd, delay) in enumerate(init_sequence):
         print(comment)
         wb.regs.sdram_dfii_pi0_address.write(a)
@@ -56,40 +46,62 @@ if sdram_initialization:
             wb.regs.sdram_dfii_pi0_command.write(cmd)
             wb.regs.sdram_dfii_pi0_command_issue.write(1)
 
-# sdram Test----------------------------------------------------------------------------------------
+# Test patterns ------------------------------------------------------------------------------------
 
-if sdram_test:
-    # hardware control
-    sdram_hardware_control()
+def seed_to_data(seed, random=True):
+    if random:
+        return (1664525*seed + 1013904223) & 0xffffffff
+    else:
+        return ((0xaa00 + seed) << 16) | (0xcc00 + seed)
 
-    def seed_to_data(seed, random=True):
-        if random:
-            return (1664525*seed + 1013904223) & 0xffffffff
+def memtest(wb, pattern, *, read=True, write=True, interleaved_rw=False, mark_scratch=True, random=True):
+    assert write or read
+
+    def ref(i):
+        return seed_to_data(i, random=random)
+
+    if not interleaved_rw and write:
+        for i, addr in enumerate(pattern):
+            wb.write(addr, ref(i))
+
+    errors = 0
+    for i, addr in enumerate(pattern):
+        if mark_scratch:
+            wb.regs.ctrl_scratch.write(i)
+        if write:
+            wb.write(addr, ref(i))
+        if read:
+            data = wb.read(addr)
+        if write and read:
+            result = "OK"
+            if data != ref(i):
+                errors += 1
+                result = "KO xor 0x{:08x}".format(data ^ ref(i))
+            print("0x{:08x}: 0x{:08x} != 0x{:08x} {}".format(addr, data, ref(i), result))
         else:
-            return seed
+            print("0x{:08x}: 0x{:08x}".format(addr, data))
 
-    def write_pattern(length):
-        for i in range(length):
-            wb.write(wb.mems.main_ram.base + 4*i, seed_to_data(i))
+    return errors
 
-    def check_pattern(length, debug=False):
-        errors = 0
-        for i in range(length):
-            error = 0
-            if wb.read(wb.mems.main_ram.base + 4*i) != seed_to_data(i):
-                error = 1
-                if debug:
-                    print("{}: 0x{:08x}, 0x{:08x} KO".format(i, wb.read(wb.mems.main_ram.base + 4*i), seed_to_data(i)))
-            else:
-                if debug:
-                    print("{}: 0x{:08x}, 0x{:08x} OK".format(i, wb.read(wb.mems.main_ram.base + 4*i), seed_to_data(i)))
-            errors += error
-        return errors
+# --------------------------------------------------------------------------------------------------
 
-    write_pattern(64)
-    errors = check_pattern(64, debug=True)
+args = parse_args()
+
+wb = RemoteClient(debug=False)
+wb.open()
+
+if not args.no_init:
+    sdram_init(wb)
+
+sdram_hardware_control()
+
+if __name__ == "__main__" and not args.no_main:
+    start = wb.mems.main_ram.base + 4 * int(args.start, 0)
+    length = 4 * int(args.length, 0)
+
+    pattern = range(start, start + length, 4)
+    errors = memtest(wb, pattern, write=not args.read_only, read=True,
+            interleaved_rw=args.interleaved, random=args.random)
     print("{} errors".format(errors))
 
-# # #
-
-wb.close()
+    wb.close()
