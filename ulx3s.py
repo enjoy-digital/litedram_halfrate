e8c1b847bad23251eed292fcd9d8679cc169db8b
Jędrzej Boczar <jboczar@antmicro.com>
2020-07-08 09:40:03 +0200

    add argument for disabling the analyzer

--- ulx3s.py
+++ ulx3s.py
@@ -73,8 +73,8 @@ class _CRG(Module):
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
-    def __init__(self, device="LFE5U-45F", toolchain="trellis",
-        sys_clk_freq=int(50e6), sdram_module_cls="MT48LC16M16", sdram_sys2x=False, **kwargs):
+    def __init__(self, device="LFE5U-45F", toolchain="trellis", sys_clk_freq=int(50e6),
+                 sdram_module_cls="MT48LC16M16", sdram_sys2x=False, with_analyzer=False, **kwargs):
 
         platform = ulx3s.Platform(device=device, toolchain=toolchain)
 
@@ -120,43 +120,44 @@ class BaseSoC(SoCCore):
         self.add_csr("leds")
 
         # Analyzer ---------------------------------------------------------------------------------
-        from litescope import LiteScopeAnalyzer
-        if not sdram_sys2x:
-            analyzer_signals = [self.sdrphy.dfi]
-        else:
-            wb_no_dat = ["cyc", "stb", "ack", "we", "adr", "err"]
-            dfi_signals = ["cas_n", "ras_n", "we_n", "address", "bank",
-                           "rddata", "rddata_en", "rddata_valid",
-                           "wrdata", "wrdata_en", "wrdata_mask"]
-            #  scratch = Signal(8)
-            #  self.comb += scratch.eq(self.ctrl._scratch.storage[:8])
-            analyzer_signals = [
-                #  self.ctrl._scratch.storage,
-                #  scratch,
-
-                self.sdrphy.dfi,
-                self.sdrphy.full_rate_phy.dfi,
-                #  *[getattr(self.sdrphy.dfi.p0, s) for s in dfi_signals],
-                #  *[getattr(self.sdrphy.dfi.p1, s) for s in dfi_signals],
-                #  *[getattr(self.sdrphy.full_rate_phy.dfi.p0, s) for s in dfi_signals],
-
-                self.sdrphy.phase_sel,
-            ]
-            if l2_size > 0:
-                analyzer_signals += [getattr(self.l2_cache.master, a) for a in wb_no_dat]
-                analyzer_signals += [getattr(self.l2_cache.slave,  a) for a in wb_no_dat]
+        if with_analyzer:
+            from litescope import LiteScopeAnalyzer
+            if not sdram_sys2x:
+                analyzer_signals = [self.sdrphy.dfi]
             else:
-                #  analyzer_signals += [self.bus.slaves["main_ram"]]
-                #  analyzer_signals += [getattr(self.bus.slaves["main_ram"],  a) for a in wb_no_dat]
-                pass
-        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals,
-            depth        = 128,
-            clock_domain = "sys2x",
-            csr_csv      = "analyzer.csv")
-        self.add_csr("analyzer")
-        print("=" * 80)
-        print("   LiteScopeAnalyzer width = {}".format(self.analyzer.data_width))
-        print("=" * 80)
+                wb_no_dat = ["cyc", "stb", "ack", "we", "adr", "err"]
+                dfi_signals = ["cas_n", "ras_n", "we_n", "address", "bank",
+                               "rddata", "rddata_en", "rddata_valid",
+                               "wrdata", "wrdata_en", "wrdata_mask"]
+                #  scratch = Signal(8)
+                #  self.comb += scratch.eq(self.ctrl._scratch.storage[:8])
+                analyzer_signals = [
+                    #  self.ctrl._scratch.storage,
+                    #  scratch,
+
+                    self.sdrphy.dfi,
+                    self.sdrphy.full_rate_phy.dfi,
+                    #  *[getattr(self.sdrphy.dfi.p0, s) for s in dfi_signals],
+                    #  *[getattr(self.sdrphy.dfi.p1, s) for s in dfi_signals],
+                    #  *[getattr(self.sdrphy.full_rate_phy.dfi.p0, s) for s in dfi_signals],
+
+                    self.sdrphy.phase_sel,
+                ]
+                if l2_size > 0:
+                    analyzer_signals += [getattr(self.l2_cache.master, a) for a in wb_no_dat]
+                    analyzer_signals += [getattr(self.l2_cache.slave,  a) for a in wb_no_dat]
+                else:
+                    #  analyzer_signals += [self.bus.slaves["main_ram"]]
+                    #  analyzer_signals += [getattr(self.bus.slaves["main_ram"],  a) for a in wb_no_dat]
+                    pass
+            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals,
+                depth        = 128,
+                clock_domain = "sys2x",
+                csr_csv      = "analyzer.csv")
+            self.add_csr("analyzer")
+            print("=" * 80)
+            print("   LiteScopeAnalyzer width = {}".format(self.analyzer.data_width))
+            print("=" * 80)
 
     def generate_sdram_phy_py_header(self):
         f = open("sdram_init.py", "w")
@@ -176,6 +177,7 @@ def main():
     parser.add_argument("--sys-clk-freq", default=50e6,           help="System clock frequency (default=50MHz)")
     parser.add_argument("--sdram-module", default="MT48LC16M16",  help="SDRAM module: MT48LC16M16, AS4C32M16 or AS4C16M16 (default=MT48LC16M16)")
     parser.add_argument("--sdram-sys2x", action="store_true",     help="Run SDRAM at double the sysclk frequency")
+    parser.add_argument("--no-analyzer", action="store_true",     help="Do not add LiteScopeAnalyzer")
     builder_args(parser)
     soc_sdram_args(parser)
     args = parser.parse_args()
@@ -183,7 +185,8 @@ def main():
     soc = BaseSoC(device=args.device, toolchain=args.toolchain,
         sys_clk_freq=int(float(args.sys_clk_freq)),
         sdram_module_cls=args.sdram_module,
-        sdram_sys2x=args.sdram_sys2x)
+        sdram_sys2x=args.sdram_sys2x,
+        with_analyzer=not args.no_analyzer)
     builder = Builder(soc, csr_csv="csr.csv")
     builder.build(run=args.build)
     soc.generate_sdram_phy_py_header()
